d9c0ff41a8a7f5d69c7a714100e02adf9fe84cc2
Damola Omotosho <damola.omotosho@gmail.com>
2017-03-17 11:20:41 -0700

    Add backend code

---
+++ JukeBox/JukeBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JukeBox.MvcApplication" Language="C#" %>
---
+++ JukeBox/JukeBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JukeBox.Startup))]
+namespace JukeBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
