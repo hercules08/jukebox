9599040b0affa815fdb62bf92ec39c7b87f0afad
Jitendra Thakur <jitenrtx@gmail.com>
2017-05-12 16:21:48 -0700

    Home Page Changed as per JukeBox

---
+++ JukeBox/JukeBox/Content/JukeBox/js/my-app.js
@@ -0,0 +1,14 @@
+﻿// Initialize your app
+var myApp = new Framework7({
+    statusbarOverlay: false
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// 3 Slides Per View, 10px Between
+var mySwiper3 = myApp.swiper('.swiper-3', {
+    pagination: '.swiper-3 .swiper-pagination',
+    spaceBetween: 10,
+    slidesPerView: 3
+});
